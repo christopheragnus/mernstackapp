2e45eb3354ebbca179401ebcc24456594348f650
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-12-18 12:24:40 +1100

    add user api routes & jwt authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin2:password2@ds137601.mlab.com:37601/devconnector"
+  mongoURI: "mongodb://admin2:password2@ds137601.mlab.com:37601/devconnector",
+  secretOrKey: "secret"
 };
